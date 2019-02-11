0665f7abad6b2efb9ac0cb5dd02633f12d973496
Allan Slocum <slocumal@flip3.engr.oregonstate.edu>
2019-02-10 22:39:20 -0800

    Random Testing Quiz added

---
+++ projects/slocumal/quiz/testme.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    char letter = "[({ })]abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[random() % 59];
+    return letter;
+}
+
+char *inputString()
+{
+	char str[5];	//initialize string
+	int size = 6;	//set size variable for looping
+	const char alphaB[] = "rest";	//available random letters
+    	if (size) {
+        	--size;
+        	for (int i = 0; i < size; i++) {	//create random string
+            		int key = rand() % (int) (sizeof alphaB - 1);
+            		str[i] = alphaB[key];
+        	}
+        	str[size] = '\0';
+    	}
+    	return str;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
